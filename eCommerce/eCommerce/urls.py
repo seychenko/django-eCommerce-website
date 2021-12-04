7987cb09e61f9586c7845e457a84aa1f26150f92
seychenko <seychenko.oleg@gmail.com>
2021-12-05 00:18:23 +0300

    Created a basic Django template
    Added project folder system and catalog application

---
+++ eCommerce/eCommerce/urls.py
@@ -0,0 +1,20 @@
+from django.urls import include
+from django.urls import path
+from django.contrib import admin
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+
+urlpatterns += [
+    path('catalog/', include('catalog.urls')),
+]
+
+urlpatterns += [
+    path('', RedirectView.as_view(url='/catalog/', permanent=True)),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
